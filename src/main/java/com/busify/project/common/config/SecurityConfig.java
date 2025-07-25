df233d83e8f1dbe43f77900e417a82b1d39d5aaa
devtion2605 <hoaicoder0503@gmail.com>
2025-07-25 13:37:51 +0700

    feat: move folder cconfig to common

--- src/main/java/com/busify/project/common/config/SecurityConfig.java
+++ src/main/java/com/busify/project/common/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.busify.project.config;
+package com.busify.project.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
