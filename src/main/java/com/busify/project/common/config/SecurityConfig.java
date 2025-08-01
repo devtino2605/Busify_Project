7d0850db89b0e402cb532782fa720fd86d787fbf
unknown <truongthuongvan203@gmail.com>
2025-08-01 12:44:23 +0700

    chore: add swagger api ui

--- src/main/java/com/busify/project/common/config/SecurityConfig.java
+++ src/main/java/com/busify/project/common/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/trips/**", "/api/routes/**", "/api/bus-operators/**", "/api/reviews/**",
-                                "/api/complaints/**")
+                                "/api/complaints/**", "swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**")
                         .permitAll());
 
         return http.build();
