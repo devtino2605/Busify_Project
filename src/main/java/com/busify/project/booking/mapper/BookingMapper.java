6bdd9326939366524e4115540a7b51ecc79bb2e3
quoc <quocla.21it@vku.udn.vn>
2025-08-11 09:41:55 +0700

    Refactor BookingMapper and TripMapper for improved readability; streamline null checks and formatting.

--- src/main/java/com/busify/project/booking/mapper/BookingMapper.java
+++ src/main/java/com/busify/project/booking/mapper/BookingMapper.java
@@ -13,7 +13,6 @@ import com.busify.project.booking.dto.response.BookingDetailResponse;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Objects;
 
 import java.util.stream.Collectors;
 
