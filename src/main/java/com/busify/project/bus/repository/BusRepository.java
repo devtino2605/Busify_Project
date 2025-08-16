a38c07f61c0e6f78edf104931d321e4e178f9668
quoc <quocla.21it@vku.udn.vn>
2025-08-16 21:05:20 +0700

    feat: enhance bus service and repository with operator-specific queries and improve trip controller structure

--- src/main/java/com/busify/project/bus/repository/BusRepository.java
+++ src/main/java/com/busify/project/bus/repository/BusRepository.java
@@ -1,19 +1,21 @@
 package com.busify.project.bus.repository;
 
 import com.busify.project.bus.entity.Bus;
-import com.busify.project.bus.enums.BusStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.busify.project.bus_operator.entity.BusOperator;
+
+
 @Repository
 public interface BusRepository extends JpaRepository<Bus, Long> {
+    List<Bus> findByOperator(BusOperator operator);
 
     @Query(value = """
         SELECT * FROM buses b
@@ -54,4 +56,3 @@ public interface BusRepository extends JpaRepository<Bus, Long> {
             Pageable pageable
     );
 }
-
