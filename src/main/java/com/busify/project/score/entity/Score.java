549fbe771e27267fbbd0d28fad385597eac5ebf3
Antoin2901 <toanna.21it@vku.udn.vn>
2025-08-29 09:55:14 +0700

    feat: add Scores table

---
+++ src/main/java/com/busify/project/score/entity/Score.java
@@ -0,0 +1,48 @@
+package com.busify.project.score.entity;
+
+import com.busify.project.booking.entity.Bookings;
+import com.busify.project.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "scores", indexes = {
+        @Index(name = "idx_scores_userId_createdAt", columnList = "user_id, created_at")
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Score {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "score_id")
+    private Long scoreId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user; // Người sở hữu điểm
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booking_id")
+    private Bookings booking;
+
+    @Column(nullable = false)
+    @Min(0)
+    private Integer points;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+}
