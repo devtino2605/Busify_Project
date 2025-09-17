e290663e15c926c7fa9525d7bd2852dae82b97f3
devtion2605 <hoaicoder0503@gmail.com>
2025-09-17 22:50:29 +0700

    Ãapply varriable enviroment for properties and update validate with promotion and campaign

--- src/main/java/com/busify/project/promotion/dto/campaign/PromotionCampaignCreateDTO.java
+++ src/main/java/com/busify/project/promotion/dto/campaign/PromotionCampaignCreateDTO.java
@@ -1,5 +1,6 @@
 package com.busify.project.promotion.dto.campaign;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -34,6 +35,7 @@ public class PromotionCampaignCreateDTO {
     private Boolean active = true;
 
     // List of promotions to create with this campaign
+    @Valid
     private List<CampaignPromotionDTO> promotions;
 
     // List of existing promotion IDs to link to this campaign
